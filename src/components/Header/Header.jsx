128ae27e7d977fa1fc26a5cee9bb2de4ff97e0ec
Tharindu Nuwantha <105153249+TharinduNuwantha@users.noreply.github.com>
2025-12-13 01:38:06 +0530

    Final header without responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,54 @@
 // myapp\src\components\Header\Header.jsx
-import menu from '../../img/menu.svg'   
-import search from '../../img/search.svg'   
-import cart from '../../img/cart-shopping.svg'   
 import '../../styles/tailwind.css'
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton, { iconButtonClasses } from '@mui/material/IconButton';
+
 
 const Header = () => {
   return (
-    <header className='w-full p-5 bg-main-background flex items-center justify-between'>
+    <header className='w-full p-4 bg-main-background flex items-center justify-between shadow-lg'>
       <div className='flex items-center'>
-        <img src={menu} alt='menu' className='w-6 h-6 object-contain cursor-pointer mr-4'/>
-        <h1 className='text-xl font-bold'>LankaDeal<span className='text-red-300'>.LK</span></h1>
-        <div className='search-container'>
+        <IconButton>
+        <MenuIcon sx={{
+            color:"white",
+            fontSize: 28,
+            cursor: "pointer",
+            '&:hover': { opacity: 0.8 }
+        }}/></IconButton>
+        <h1 className='text-xl font-bold ml-3'>LankaDeal<span className='text-red-300'>.LK</span></h1>
+        <div className='search-container ml-6'>
           <input 
             type='text' 
-            placeholder='Search products...' 
-            className='outline-none p-2 font-medium text-base w-[300px] bg-inherit' 
+            placeholder='Search for products, brands and more...' 
+            className='outline-none p-2 font-medium text-sm w-[400px] bg-inherit' 
           />
-          <img src={search} alt='search' className='w-5 h-5 object-contain cursor-pointer'/>
+          <SearchIcon sx={{
+            color: '#ff4747',
+            fontSize: 22,
+            cursor: "pointer"
+          }}/>
         </div>
       </div>
       <div className='flex items-center gap-6'>
-        <button className='btn-primary'>
-          Login
-        </button>
-        <div className='relative'>
-          <img src={cart} alt='cart' className='w-7 h-7 object-contain cursor-pointer'/>
-          <span className='absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center'>
+        <div className='flex items-center gap-2 cursor-pointer'>
+          <PersonIcon sx={{
+            color: 'white',
+            fontSize: 22
+          }}/>
+          <span className='text-white font-medium hover:text-accent-yellow transition duration-300'>
+            Login / Sign Up
+          </span>
+        </div>
+        <div className='relative cursor-pointer'>
+          <ShoppingCartIcon sx={{
+            color:'white',
+            fontSize: 26,
+            '&:hover': { transform: 'scale(1.1)' }
+          }}/>
+          <span className='cart-badge absolute -top-2 -right-2 text-xs rounded-full w-5 h-5 flex items-center justify-center font-bold'>
             3
           </span>
         </div>
