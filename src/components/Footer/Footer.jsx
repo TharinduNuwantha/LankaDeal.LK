a66e27962db4a9421321eff09359db72d3b7028c
Tharindu Nuwantha <105153249+TharinduNuwantha@users.noreply.github.com>
2025-12-14 00:28:55 +0530

    footer updates 1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,20 +5,27 @@ import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined
 
 const Footer = () => {
   return (
-    <footer className='fixed bottom-0 left-0 z-[100] w-full py-3 px-2 bg-slate-400 flex items-center justify-between' >
-      <IconButton>
-        <HomeOutlinedIcon/>
-      </IconButton>
+    <footer className='fixed bottom-0 left-0 z-[100] w-full p-3 bg-slate-300 flex items-center justify-between' >
 
-      <IconButton>
-        <AppsOutlinedIcon/>
-      </IconButton>
-
-      <IconButton>
-        <AccountCircleOutlinedIcon/>
-      </IconButton>
+    <FooterIcon Icon={HomeOutlinedIcon} text={"Home"}/>
+    <FooterIcon Icon={AppsOutlinedIcon} text={"Category"}/>
+    <FooterIcon Icon={AccountCircleOutlinedIcon} text={"Account"}/>
     </footer>
   )
 }
 
 export default Footer
+
+const FooterIcon = ({Icon,text}) =>(
+  <IconButton sx={{
+    padding:"4px",
+    paddingTop:'0px',
+    borderRadius:"5px",
+    color:"red"
+  }}>
+    <div className='text-gray-700 hover:text-[#f54c4c] transition-all duration-300'>
+      <Icon/>
+        <p className='text-xs font-semibold'>{text}</p>
+    </div>
+  </IconButton>
+) 
