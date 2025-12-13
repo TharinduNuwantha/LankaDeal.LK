c30a9ef7dd94310dd5cf4586678cbe2114bb0714
Tharindu Nuwantha <105153249+TharinduNuwantha@users.noreply.github.com>
2025-12-13 21:55:24 +0530

    footer step 1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import IconButton from '@mui/material/IconButton';
+import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 const Footer = () => {
   return (
-    <footer className='fixed bottom-0 left-0 z-[100] w-full py-5 px-2 bg-slate-400 flex items-center justify-between' ></footer>
+    <footer className='fixed bottom-0 left-0 z-[100] w-full py-3 px-2 bg-slate-400 flex items-center justify-between' >
+      <IconButton>
+        <HomeOutlinedIcon/>
+      </IconButton>
+
+      <IconButton>
+        <AppsOutlinedIcon/>
+      </IconButton>
+
+      <IconButton>
+        <AccountCircleOutlinedIcon/>
+      </IconButton>
+    </footer>
   )
 }
 
