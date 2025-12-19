179bf260afe4997f66e84543a0d0fb4417428dce
Tharindu Nuwantha <105153249+TharinduNuwantha@users.noreply.github.com>
2025-12-19 11:13:56 +0530

    login logout simple testing with fierbase

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -13,116 +13,52 @@ const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  useEffect(()=>{
 
-// const auth = getAuth();
-// onAuthStateChanged(auth, (user) => {
-//   if (user) {
-
-//     const uid = user.uid;
-//     console.log(user);
-    
-//     // ...
-//   } else {
-//     // User is signed out
-//     // ...
-//   }
-// });
-
-
-
-
-
-
-    // const authCheack = ()=> {
-    //   onAuthStateChanged(auth, (user) => {
-    //   if (user) {
-    //     // User is signed in, see docs for a list of available properties
-    //     // https://firebase.google.com/docs/reference/js/auth.user
-    //     const uid = user.uid;
-    //     console.log(uid);
-        
-    //     // ...
-    //   } else {
-    //     // User is signed out
-    //     // ...
-    //   }
-    // });
-    // }
-    // authCheack()
-    // return()=>{
-    //   authCheack()
-    // }
-  },[])
 
 
   useEffect(()=>{
-    const fun = ()=>{
-    const user = auth.currentUser;
-      if (user) {
-
-        console.log("current user -> ",user)
-      } else {
-       console.log("no user");
-       
-      }
+    const UserCheackFun = ()=>{
+          onAuthStateChanged(auth, (user) => {
+              if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/auth.user
+                const uid = user.uid;
+                console.log(user);
+                
+                // ...
+              } else {
+                console.log('user error');
+                
+              }
+            });
     }
-    fun();
-    setTimeout(()=>{
-      fun()
-    },[1000])
+    
+    UserCheackFun();
+
+    return()=> UserCheackFun()
+
   },[])
 
 
   const loginSubmit = ()=>{
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
-        const uid = user.uid;
-        console.log(user);
-        
-        // ...
-      } else {
-        // User is signed out
-        // ...
-      }
+
+    const email = usernameRef.current.value;
+    const password = passwoardRef.current.value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user);
+      
+      // ...
+    })
+    .catch((error) => {
+      // const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      
     });
 
-
-
-  //   const email = usernameRef.current.value;
-  //   const password = passwoardRef.current.value;
-  // signInWithEmailAndPassword(auth, email, password)
-  //   .then((userCredential) => {
-  //     // Signed in 
-  //     const user = userCredential.user;
-  //     console.log(user);
-      
-  //     // ...
-  //   })
-  //   .catch((error) => {
-  //     // const errorCode = error.code;
-  //     const errorMessage = error.message;
-  //     console.log(errorMessage);
-      
-  //   });
-
-
-    // const q = query(collection(db, "users"), where("userName", "==", "admin"));
-
-    //   getDocs(q).then((querySnapshot) => {
-    //           querySnapshot.forEach((doc) => {
-    //                       const data = doc.data();
-    //                       console.log(doc.id, " => ", data);
-    //                       dispatch(addUser({ ...data, isAdmin: true }));
-    //                     });
-    //                     navigate("/admin");
-    //     })
-    //     .catch((error) => {
-    //         console.log("Error getting documents: ", error);
-    //     });
   }
 
   return (
