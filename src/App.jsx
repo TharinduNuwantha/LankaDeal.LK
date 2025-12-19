2e9efdec1bc65e5cdab398566c0ebea4c8109c93
Tharindu Nuwantha <105153249+TharinduNuwantha@users.noreply.github.com>
2025-12-19 19:34:47 +0530

    login user

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useEffect } from 'react';
 import './App.css';
 
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import AppRouters from './Routers/AppRouters';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './FireBase/firebase';
 
 
 function App() {
+  useEffect(()=>{
+    const userCheack = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        // ...
+      } else {
+        // User is signed out
+        console.log("no user");
+        
+      }
+    });
+    return()=> userCheack
+  },[])
   return (
     <div>
       <AppRouters/>
